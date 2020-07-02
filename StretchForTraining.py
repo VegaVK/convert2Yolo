979dae38c867254da0cc068c7b9f775627b8d9f1
vamsikrishna95@gmail.com <44810844+VegaVK@users.noreply.github.com>
2020-07-02 14:45:01 -0500

    Removing dog class from taining data

--- StretchForTraining.py
+++ StretchForTraining.py
@@ -8,7 +8,7 @@ import random
 
 # Format for annotations: <object-class> <x_center> <y_center> <width> <height>
 # img =cv2.imread('/home/mkz/FLIR_test/FLIR_00001.jpeg', cv2.IMREAD_GRAYSCALE)
-onlyfiles = [f for f in listdir("/home/vamsi/ToDel_FLIR/video/thermal_8_bit/") if isfile(join("/home/vamsi/ToDel_FLIR/video/thermal_8_bit/", f))]
+onlyfiles = [f for f in listdir("/home/vamsi/ToDel_FLIR/training/thermal_8_bit/") if isfile(join("/home/vamsi/ToDel_FLIR/training/thermal_8_bit/", f))]
 # cv2.imshow('image',img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
@@ -19,8 +19,8 @@ onlyfiles = [f for f in listdir("/home/vamsi/ToDel_FLIR/video/thermal_8_bit/") i
 print('No. of Files: ', len(onlyfiles))
 ManF = open("/home/vamsi/ToDel_FLIR/manifest.txt","a+")
 for idx in range(len(onlyfiles)):
-    Input_img =cv2.imread('/home/vamsi/ToDel_FLIR/video/thermal_8_bit/'+onlyfiles[idx], cv2.IMREAD_GRAYSCALE)
-    for sdx in range(3): # Supplemental Files
+    Input_img =cv2.imread('/home/vamsi/ToDel_FLIR/training/thermal_8_bit/'+onlyfiles[idx], cv2.IMREAD_GRAYSCALE)
+    for sdx in range(1): # Supplemental Files
         OutputImg=np.zeros((512,512*5))
         randomPos=random.randint(1,512*5-642)
         OutputImg[:,randomPos:randomPos+640]=Input_img
@@ -28,7 +28,7 @@ for idx in range(len(onlyfiles)):
         cv2.imwrite('/home/vamsi/ToDel_FLIR/Stretched/'+SupplFileName,OutputImg)
         print(onlyfiles[idx],sdx)
         ### Create Text File names
-        tempFileName='/home/vamsi/ToDel_FLIR/video/converted/thermal_8_bit/'+onlyfiles[idx][0:-5]+'.txt'
+        tempFileName='/home/vamsi/ToDel_FLIR/training/converted/thermal_8_bit/'+onlyfiles[idx][0:-5]+'.txt'
         if os.path.isfile(tempFileName):
             # data/flirData/training/thermal_8_bit/
             ManifestLine='data/flirData/training/thermal_8_bit/'+onlyfiles[idx][0:-5]+'a'+str(sdx)+'.jpeg'+'\n'
@@ -37,6 +37,10 @@ for idx in range(len(onlyfiles)):
                 with open('/home/vamsi/ToDel_FLIR/Annotations_Str/'+onlyfiles[idx][0:-5]+'a'+str(sdx)+'.txt', "w+") as DestF:
                     for line in SourceF:
                         l = line.split()
+                        if l[0]=='3':
+                            print('FOUND DOGGY!')
+                            # exit()
+                            continue # To next line
                         objClass=l[0] # Unchanged
                         Box_x=(float(l[1])*640+randomPos)/(512*5)
                         Box_x=str(Box_x) # Convert to string
